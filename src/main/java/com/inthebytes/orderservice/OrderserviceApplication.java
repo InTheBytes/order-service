6396907fbb146f6f0d038b25f735fee1ab1ab676
mosaab-ss <80281522+mosaab-ss@users.noreply.github.com>
2021-06-06 11:28:01 -0600

    ITB-197: added jenkins pipeline, CFT, CORS and openapi infrastructure

--- src/main/java/com/inthebytes/orderservice/OrderserviceApplication.java
+++ src/main/java/com/inthebytes/orderservice/OrderserviceApplication.java
@@ -2,12 +2,28 @@ package com.inthebytes.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OrderserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderserviceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOriginPatterns("https://*.stacklunch.com", "https://stacklunch.com")
+						.allowCredentials(true)
+						.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+			}
+		};
 	}
 
 }
